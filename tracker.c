c4ca390a96f6176a564b2e63357366e6a5492b9f
Valente2 <valentevescio@gmail.com>
2025-08-30 08:29:50 -0400

    Added bond calculation

--- tracker.c
+++ tracker.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <math.h>
 
+
+
 int main(int argc, char *argv[]) {
     if (argc == 1) {
         printf("Enter your calcution such as:\n-debt  (Enter your income and expences to find out wheter you are in debt)\n-bond (Enter money put in, and interst rate(per year) and length)\n -mortgage (Enter the house price, down payment, interst rate(per year) and length)\n -loan (Enter the loan amount, interst rate(per year) and length)\n");
@@ -10,20 +12,36 @@ int main(int argc, char *argv[]) {
     }
     if(argc < 2) {
         printf("Not enough arguments\n");
-        return 1;
+        return 0;
     }
-    if(argv[1] == -debt){
+    if(strcmp(argv[1], "-debt") == 0) {
         if(argc != 4) {
             printf("Not enough arguments for debt calculation\n");
             return 1;
         }
         double income = atof(argv[2]);
         double expenses = atof(argv[3]);
+        double debt = expenses - income;
         if(income > expenses) {
             printf("You are not in debt\n");
         } else {
             printf("Uh-oh you are in debt :(\n");
+            printf("Your debt amount is: %.2f\n", debt);
         }
+        return 0;
+    }
+    if(strcmp(argv[1], "-bond") == 0) {
+        if(argc != 5) {
+            printf("Not enough arguments for bond calculation\n");
+            return 1;
+        }
+        double principal = atof(argv[2]);
+        double annual_rate = atof(argv[3]) / 100.0;
+        double years = atof(argv[4]);
+        double monthly_rate = annual_rate / 12.0;
+        double months = years * 12.0;
+        double payment = (principal * monthly_rate) / (1 - pow(1 + monthly_rate, -months));
+        printf("Your monthly bond payment is: %.2f\n", payment);
         return 0;
     }
 }
