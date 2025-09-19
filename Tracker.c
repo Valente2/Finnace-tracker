9ace255697c58e6c7b1a09cba3401b3bdd6c38ec
Valente2 <valentevescio@gmail.com>
2025-09-19 10:03:54 -0400

    fixed bugs and stuff

--- Tracker.c
+++ Tracker.c
@@ -6,18 +6,25 @@
 #define flagfile "fiist_run.flag"
 
 int main(){
-
-    FILE *fp;
+    double income, expense;
+    char inc[11], exp[11];
+    char *fp;
+    int fplen = 20;
+    fp = (char *)malloc(fplen * sizeof(char));
+    
     
     fp = fopen(flagfile, "r");
     if(fp == NULL){
-        printf("This is your first run.\nThis program will track your fincance\nThe idea is you enter your income and expenses\nAnd the program will track it for you in a  file called  'fin.txt' (another file that should not be delete is first_run.flag)\n");
+        printf("This is your first run.\nThis program will track your fincance\nThe idea is you enter your income and expenses\nAnd the program will track it for you in a  file called  'income' (another file that should not be delete is first_run.flag)\n");
         fp = fopen(flagfile, "w");
         fclose(fp);
+        fp = fopen("income", "w");
     }
     else{
         printf("Welcome back!\n");
         fclose(fp);
+        fp = fopen("income", "w");
     }
+        
 
 }
